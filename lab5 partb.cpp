3b1bd69ed3a228aa8cd763376c79bfbe10e46d21
Serkantekin1000 <serkan.tekin@dawsoncollege.qc.ca>
2026-09-21 15:03:11 -0400

    Add files via upload
    corrections made over time

---
+++ lab5 partb.cpp
@@ -0,0 +1,71 @@
+
+#include <Arduino.h>
+#include <SPI.h>
+#include <SD.h>
+const int chipSelect = 10;
+int position = 0;
+File dataFile; 
+const int potPin = A0;
+int sensorValue = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Program Started");
+  Serial.println("Initializing SD Card...");
+  if(SD.begin(chipSelect)) {
+    Serial.println("SD Card Found");
+  } 
+  else {
+    Serial.println("SD Card Not Found");
+  }
+  
+  dataFile = SD.open("sensor.csv", FILE_WRITE);
+  
+
+//  dataFile = SD.open("hello.txt", FILE_WRITE);
+//   dataFile.println("Hello Dawson");
+//   dataFile.close();
+// // CSV file and write header 
+// dataFile = SD.open("senor.txt", FILE_WRITE);
+//  dataFile.println(sensorValue); 
+//  dataFile.close(); 
+// // Save:
+// Value = sensorValue
+
+// Close file
+  if (dataFile) 
+  {
+    dataFile.println("Time,Value");
+    dataFile.close();
+    Serial.println("CSV Header Written");
+  }else {
+    Serial.println("Failed to createfile");
+  }
+  delay(500);
+}
+ 
+void loop() {
+  // Serial.println(position);
+  sensorValue = analogRead(potPin);
+
+  Serial.print("Value = ");
+  Serial.println(sensorValue);
+  unsigned long timeSeconds = millis() / 1000;  
+  
+  Serial.print("Time: ");
+  Serial.print(timeSeconds);
+  Serial.println(" s ");
+
+  dataFile = SD.open("sensor.csv", FILE_WRITE);
+ 
+ if (dataFile) {
+    dataFile.print(timeSeconds);
+    dataFile.print(",");
+    dataFile.println(sensorValue); 
+    dataFile.close();
+    Serial.println("Data Saved");
+  }else {
+    Serial.println("Error Opening File");
+  }
+  delay(1000);
+}
